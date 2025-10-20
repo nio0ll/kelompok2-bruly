752bf7c883f5054f46cf03abdfd48cbf0e09e03b
nio0ll <ranianuril210@gmail.com>
2025-10-20 23:51:25 +0700

    siswa.cpp v0

---
+++ siswa.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+//tipe nilai
+struct nilai{
+    float mtk;
+    float bin;
+    float bing;
+    float ipa;
+};
+
+//tipe data siswa
+struct siswa{
+    string nama;
+    string nisn;
+    string jurusan;
+    nilai tnilai;
+};
+
+//tambah data pada file
+void adddata(){}
+
+//membaca data dari file dan di tampilkan
+void outputdata(){}
+
+//searching berdasarkan nisn
+void searchdata(){}
+
+//rumus nilaitotal = mtk*0.4 + ipa*0.3 + bin*0.2 + bing*0.2
+float nilaitotal(){}
+
+//menampilkan data berdasarkan rankingnya
+void rank(){}
+
+int main (){
+
+    return 0;
+}
